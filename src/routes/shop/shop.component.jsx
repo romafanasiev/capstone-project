cb4dd722a91c3203664dcddde2f81986714600fe
Roma <roma@MacBook-Pro-Roman.local>
2022-08-26 11:18:33 +0300

    made refactoring for categories reducer

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -6,20 +6,18 @@ import {
   getCategoriesAndDocuments,
 } from "../../utils/firebase/firebase.utils";
 
-import { setCategoriesMap } from "../../store/categories/category.actions";
+import { setCategories } from "../../store/categories/category.actions";
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-
-import "./shop.styles.scss";import "./shop.styles.scss";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoryMap));
+      const categoriesArray = await getCategoriesAndDocuments();
+      dispatch(setCategories(categoriesArray));
     };
 
     getCategoriesMap();
